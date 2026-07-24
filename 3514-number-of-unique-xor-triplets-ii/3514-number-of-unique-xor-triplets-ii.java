4caab27ac05ce3e1d865231adf1775cde32cf0d4
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2026-07-25 02:38:00 +0530

    Time: 132 ms (98.5%), Space: 47.2 MB (17.29%) - LeetHub

---
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        boolean[] one = new boolean[2048];
+        boolean[] two = new boolean[2048];
+        boolean[] three = new boolean[2048];
+
+        // XOR of one element
+        for (int num : nums) {
+            one[num] = true;
+        }
+
+        // XOR of two elements
+        for (int x = 0; x < 2048; x++) {
+            if (!one[x]) continue;
+
+            for (int num : nums) {
+                two[x ^ num] = true;
+            }
+        }
+
+        // XOR of three elements
+        for (int x = 0; x < 2048; x++) {
+            if (!two[x]) continue;
+
+            for (int num : nums) {
+                three[x ^ num] = true;
+            }
+        }
+
+        int ans = 0;
+        for (boolean b : three) {
+            if (b) ans++;
+        }
+
+        return ans;
+    }
+}
