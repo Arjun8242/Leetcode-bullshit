ac2ea4a6613f5fdda53085ac3b6798797872eee7
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2026-08-13 00:39:51 +0530

    Time: 893 ms (5.23%), Space: 283.1 MB (5.8%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,36 +1,16 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> ans=new ArrayList<>();
-        Map<String, Integer> map=new HashMap<>();
+        List<String> list=new ArrayList<>();
+        HashMap<String, Integer> map=new HashMap<>();
 
-        int left=0;
-        StringBuilder curr=new StringBuilder();
-        for(int right=0;right<s.length();right++){
+        for(int i=0;i<=s.length()-10;i++){
+            map.put(s.substring(i, i+10), map.getOrDefault(s.substring(i, i+10), 0)+1);
 
-            curr.append(s.charAt(right));
-
-            if(right-left+1==10){
-                String key=curr.toString();
-
-                if(map.getOrDefault(key, 0) == 1) ans.add(key);
-
-                map.put(key, map.getOrDefault(key, 0) + 1);
-
-                curr.deleteCharAt(0);
-                left++;
+            if(map.get(s.substring(i, i+10))==2){
+                list.add(s.substring(i, i+10));
             }
         }
+        return list;
 
-        // for (int i = 0; i <= s.length() - 10; i++) {
-        //     String sub = s.substring(i, i + 10);
-        //     map.put(sub, map.getOrDefault(sub, 0) + 1);
-        //     if (map.get(sub) == 2) ans.add(sub); 
-        // }
-        // simple logic 
-        // iterate from 0 to len-10
-        // take substring of len 10 and check if it is present in map or not 
-        // if present increase the count and store it in ans list
-
-        return ans;
     }
 }
