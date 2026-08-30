e1ead37c6185a9d541e01a73b651cff9d26fd372
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2026-08-31 00:06:53 +0530

    Time: 1949 ms (5.1%), Space: 54.8 MB (5.8%) - LeetHub

--- 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.java
+++ 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.java
@@ -2,9 +2,7 @@ class Solution {
     public int minOperations(int[] nums, int sum) {
         int n=nums.length;
         int[][] dp=new int[n+1][sum+1];
-        for(int i=0;i<=n;i++){
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-        }
+        for(int[] row:dp)Arrays.fill(row, Integer.MAX_VALUE);
 
         dp[0][0]=0;
 
