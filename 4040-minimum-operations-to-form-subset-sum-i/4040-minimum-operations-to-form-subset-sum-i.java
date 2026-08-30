762177218d78ee453dcb443a2a7c3719d2ac4a85
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2026-08-31 00:06:09 +0530

    Time: 1648 ms (5.1%), Space: 54.5 MB (5.8%) - LeetHub

---
+++ 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int minOperations(int[] nums, int sum) {
+        int n=nums.length;
+        int[][] dp=new int[n+1][sum+1];
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+
+        dp[0][0]=0;
+
+        for(int i=0;i<n;i++){
+            List<int[]> possible=generate(nums[i], sum);
+
+            for(int s=0;s<=sum;s++){
+                if(dp[i][s]==Integer.MAX_VALUE) continue;
+
+                //don't pick
+                dp[i+1][s]=Math.min(dp[i+1][s], dp[i][s]);
+
+                //pick
+                for(int[] 
+                p:possible){
+                    int val=p[0];
+                    int ops=p[1];
+
+                    if(s+val<=sum){
+                        dp[i+1][s+val]=Math.min(dp[i+1][s+val], dp[i][s]+ops);
+                    }
+                }
+            }
+        }
+
+        return dp[n][sum] == Integer.MAX_VALUE ? -1 : dp[n][sum];
+    }
+    public List<int[]> generate(int num, int sum){
+        int multiply=0;
+        List<int[]> list=new ArrayList<>();
+        int val=num;
+
+        while(true){
+            int x=val;
+            int divide=0;
+            while(x>0){
+                list.add(new int[]{x, multiply+divide});
+
+                x/=2;
+                divide++;
+            }
+
+            if(val>sum)break;
+
+            val*=2;
+            multiply++;
+        }
+        return list;
+    }
+}
